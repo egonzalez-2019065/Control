05c6f820a2f926e140c5fb5eea9332ab56e1d7e1
egonzalez-2019065 <egonzalez-2019065@kinal.edu.gt>
2024-07-17 12:18:56 -0600

    Elimino mal manejo de archivos

--- Control/index.js
+++
@@ -1,7 +0,0 @@
-import { initServer } from "./configs/app.js";
-import { connect } from "./configs/mongo.js";
-import { insertDefault } from "./src/user/user.controller.js";
-
-initServer()
-connect()
-insertDefault()
